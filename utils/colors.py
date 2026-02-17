8a80b11609b71d42b468ba93636d2cb885be9515
BackdoorCreatorWorld <backdoorcreator@gmail.com>
2026-02-17 15:34:25 +0700

    Create colors.py

---
+++ utils/colors.py
@@ -0,0 +1,16 @@
+"""
+Color definitions
+"""
+
+class Colors:
+    RESET = '\033[0m'
+    BLACK = '\033[30m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
